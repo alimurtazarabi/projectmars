0939aa7f9f66029e743089693715b00e07d9c155
Ali Murtaza <khattaksa345@gmail.com>
2019-12-19 21:50:16 +0100

    completed

--- src/services/fakeApiService.jsx
+++ src/services/fakeApiService.jsx
@@ -1,38 +1,52 @@
 const firms = [
 	{
         "id": 1,
-        "name": "Martian Firma",
+        "name": "Food Firma",
         "budget": 1000000,
         "budget_spent": 45000,
         "date_of_first_purchase": "2020-03-01"
     },
     {
         "id": 2,
-        "name": "Solar Firma",
+        "name": "Automotive Firma",
         "budget": 2000000,
         "budget_spent": 55000,
         "date_of_first_purchase": "2020-02-01"
     },
     {
         "id": 3,
-        "name": "Lunar Firma",
+        "name": "Health Firma",
         "budget": 13000000,
         "budget_spent": 65000,
         "date_of_first_purchase": "2020-03-02"
     },
     {
         "id": 4,
-        "name": "Galaxy Firma",
+        "name": "Entertainment Firma",
         "budget": 7000000,
         "budget_spent": 75000,
         "date_of_first_purchase": "2020-04-03"
     },
     {
         "id": 5,
-        "name": "Earth Internation Firma",
+        "name": "Digital Internation Firma",
         "budget": 8000000,
         "budget_spent": 85000,
         "date_of_first_purchase": "2020-03-07"
+    },
+    {
+        "id": 6,
+        "name": "Clothing Firma",
+        "budget": 9000000,
+        "budget_spent": 78000,
+        "date_of_first_purchase": "2020-03-08"
+    },
+    {
+        "id": 7,
+        "name": "Clothing Firma",
+        "budget": 9500000,
+        "budget_spent": 63000,
+        "date_of_first_purchase": "2020-03-21"
     }
 ];
 
--- src/services/fetchDataService.jsx
+++ src/services/fetchDataService.jsx
@@ -1,5 +1,6 @@
 import { apiUrl } from "../config.json";
 
+// for some reasons json-server didn't run with me so i just made a fake api service in adjecent file.
 export function getFirms() {
   return http.get(apiUrl);
 }
